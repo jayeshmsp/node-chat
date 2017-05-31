44e9ba584652d6072ad6e2d3b6dea075108a12ba
jayeshmsp <jayesh@msp-group.co.uk>
2017-05-31 12:52:41 +0530

    add address colom to user table

--- database/migrations/2017_05_31_045957_add_first_last_name_in_usr.php
+++ database/migrations/2017_05_31_045957_add_first_last_name_in_usr.php
@@ -16,6 +16,7 @@ class AddFirstLastNameInUsr extends Migration
         Schema::table('users', function ($table) {
             $table->string('first_name')->nullable();
             $table->string('last_name')->nullable();
+            $table->text('address')->nullable();
         });
     }
 
